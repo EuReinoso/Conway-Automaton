b32cf0493590f74b5a521d8d8f613b18a563677b
EuReinoso <lucas.reinoso.br@gmail.com>
2021-04-07 17:33:58 -0300

    Generating rects of grid

--- main.py
+++ main.py
@@ -1,20 +1,41 @@
 import pygame,sys
-
+from numpy import empty
 pygame.init()
 
-WINDOW_SIZE = (640,480)
+BLACK = (0, 0, 0)
+WHITE = (200, 200, 200)
+
+WINDOW_SIZE = (640, 480)
+TILE_SIZE = 10
+
+screen = pygame.Surface(WINDOW_SIZE)
+
+grid_list = empty(shape=[round(WINDOW_SIZE[0]/TILE_SIZE), round(WINDOW_SIZE[1]/TILE_SIZE)], 
+                        dtype= object)
+
+def gen_rects():
+    x = 0
+    for i in range(0,screen.get_size()[0],TILE_SIZE):
+        y = 0
+        for j in range(0, screen.get_size()[1], TILE_SIZE):
+            rect = pygame.Rect(i, j, TILE_SIZE, TILE_SIZE)
+            grid_list[x,y] = rect
+            y += 1
+        x += 1
 
 window = pygame.display.set_mode(WINDOW_SIZE)
 pygame.display.set_caption('Conways Game')
 
-
+gen_rects()
 while True:
 
-    window.fill((0,0,0))
+    screen.fill(BLACK)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
     
+    
+    window.blit(screen,(0, 0))
     pygame.display.update()
