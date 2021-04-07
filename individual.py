4e1cb2b08e9437e55fbd06bfbaff07c36e882baf
EuReinoso <lucas.reinoso.br@gmail.com>
2021-04-07 17:52:55 -0300

    Individual class

---
+++ individual.py
@@ -0,0 +1,7 @@
+from square import Square
+
+class Individual:
+    def __init__(self,rect)
+        self.square = Square(rect)
+        self.alive = False
+        self.adjacents = []
--- square.py
+++ square.py
@@ -1,4 +1,5 @@
 class Square:
     def __init__(rect):
         self.rect = rect
+        self.color = (0, 0, 0)
     
