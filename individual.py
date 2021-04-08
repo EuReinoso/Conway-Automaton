8f2aa9133a178bb5b1cb6791553e79a5a88901e4
EuReinoso <lucas.reinoso.br@gmail.com>
2021-04-08 16:58:43 -0300

    Individual is_alive verification

--- individual.py
+++ individual.py
@@ -3,3 +3,17 @@ class Individual:
         self.rect = rect
         self.alive = False
         self.adjacents = []
+
+    def is_alive(self):
+        count = 0
+        for adj in self.adjacents:
+            if adj.alive:
+                count += 1
+        if self.alive:
+            if count < 2:
+                self.alive = False
+            if count > 3:
+                self.alive = False
+        else:
+            if count == 3:
+                self.alive = True
